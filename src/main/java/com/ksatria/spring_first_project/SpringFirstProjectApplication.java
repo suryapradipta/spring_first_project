b07ae0e922bbb6ba5fecc47efd2b3272f593a74e
suryapradipta.github.io <63577853+suryapradipta@users.noreply.github.com>
2024-02-16 09:19:59 +0800

    feat: write rest end point

--- src/main/java/com/ksatria/spring_first_project/SpringFirstProjectApplication.java
+++ src/main/java/com/ksatria/spring_first_project/SpringFirstProjectApplication.java
@@ -2,12 +2,21 @@ package com.ksatria.spring_first_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringFirstProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringFirstProjectApplication.class, args);
+    }
+
+    // this request URI method
+    @RequestMapping(value = "/")
+    public String hello() {
+        return "Hello surya!";
     }
 
 }
