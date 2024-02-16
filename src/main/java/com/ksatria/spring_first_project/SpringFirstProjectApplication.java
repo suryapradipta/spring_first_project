061f1f1760571f75004df6aabccdcace4c9e2515
suryapradipta.github.io <63577853+suryapradipta@users.noreply.github.com>
2024-02-16 09:47:42 +0800

    latest progress

--- src/main/java/com/ksatria/spring_first_project/SpringFirstProjectApplication.java
+++ src/main/java/com/ksatria/spring_first_project/SpringFirstProjectApplication.java
@@ -2,9 +2,15 @@ package com.ksatria.spring_first_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringFirstProjectApplication {
+public class SpringFirstProjectApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringFirstProjectApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringFirstProjectApplication.class, args);
